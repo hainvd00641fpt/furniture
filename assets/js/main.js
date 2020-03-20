a023bcac7d34c16647428ef3167202273d794f42
hainvd00641fpt <35477822+hainvd00641fpt@users.noreply.github.com>
2020-03-20 23:09:17 +0700

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ $('#dropdownCart').mouseenter(function(){
         $('.popup-cart').show()
       $('.popup-notify').hide()
       } else {
-        $box.css("opacity", 1);
+        // $box.css("opacity", 1);
       }
     });
   });
@@ -31,33 +31,3 @@ $('#dropdownCart').mouseenter(function(){
         });
  });
 
- function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
